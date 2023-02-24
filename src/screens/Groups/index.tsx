9103c1ed5c224db936b498fdd4194b773d12ea7e
vitorhugomarcal <vitorhugomarcal@gmail.com>
2023-02-24 00:35:54 -0300

    finish components Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 
 export default function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title='Turmas' subtitle='jogue com a sua turma' />
     </Container>
   );
 }
