fa1368c8cdef565935bf82a5d3203afdf18980c3
vitorhugomarcal <vitorhugomarcal@gmail.com>
2023-02-24 00:26:27 -0300

    finish component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 
 import logoImg from '@assets/logo.png'
 
-export function Header() {
+interface Props {
+  showBackButton?: boolean
+}
+
+export function Header({showBackButton = false}: Props) {
   return (
     <Container>
+      {
+        showBackButton &&
+      <BackButton>
+        <BackIcon />
+      </BackButton>
+      }
       <Logo source={logoImg}/>
     </Container>
   )
