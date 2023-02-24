c030d2041d4392ee9be43b198f299c44badc8860
vitorhugomarcal <vitorhugomarcal@gmail.com>
2023-02-24 01:08:00 -0300

    finish components ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export default function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Rocketseat', 'Galera do Ignite', 'Familia' ])
+  // const [groups, setGroups] = useState<string[]>(['Rocketseat', 'Galera do Ignite', 'Familia' ])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -19,6 +21,12 @@ export default function Groups() {
         renderItem={({ item }) => (
           <GroupCard
             title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message='Que tal cadastrar a primeira turma ?'
           />
         )}
       />
