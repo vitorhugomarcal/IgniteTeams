ec3e95708661ad18e4ec23e2e9b279f04b411374
vitorhugomarcal <vitorhugomarcal@gmail.com>
2023-02-24 15:42:03 -0300

    refactory css

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,17 +1,20 @@
 import { TextInput } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(TextInput)`
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD};
+
+    background-color: ${theme.COLORS.GRAY_700};
+    color: ${theme.COLORS.WHITE};
+  `}
   flex: 1;
 
   min-height: 56px;
   max-height: 56px;
 
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD};
   
-  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-  color: ${({ theme }) => theme.COLORS.WHITE};
 
   border-radius: 6px;
   padding: 16px;
--- src/components/ListEmpty/styles.ts
+++ src/components/ListEmpty/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
@@ -6,9 +6,12 @@ export const Container = styled.View`
   align-items: center;
 `
 export const Message = styled.Text`
+ ${({ theme }) => css`
+  font-size: ${theme.FONT_SIZE.SM};
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+  color: ${theme.COLORS.GRAY_300};
+ `}
   text-align: center;
   
-  font-size: ${({ theme }) => theme.FONT_SIZE.SM};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  color: ${({ theme }) => theme.COLORS.GRAY_300};
+  
 `
