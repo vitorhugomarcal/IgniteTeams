ae1a71fc9207e85e8d4036a588b1c235ec0efa0c
vitorhugomarcal <vitorhugomarcal@gmail.com>
2023-02-24 00:59:16 -0300

    add FlatList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,27 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Rocketseat', 'Galera do Ignite', 'Familia' ])
+
   return (
     <Container>
       <Header />
       <Highlight title='Turmas' subtitle='jogue com a sua turma' />
-      <GroupCard title='Galera do Ignite' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+      />
     </Container>
   );
 }
